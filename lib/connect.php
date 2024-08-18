ea0610d6f3e9b76ddf1c995b354ea955e6d51f0b
MDVR9980 <mdvahhabrajaee@gmail.com>
2024-08-18 21:52:23 +0330

    Updated

--- lib/connect.php
+++ lib/connect.php
@@ -1,71 +1,30 @@
 <?php  
-define("DB_SERVICE", "localhost");  
-define("DB_USER", "root");  
-define("DB_PASS", "");  
-define("DB_DATABASE", "university");  
-
-class mySql {  
+class Database {  
     private $conn;  
 
-    public function __construct() {  
-        $this->connect();  
-    }  
-
-    private function connect() {  
-        $this->conn = mysqli_connect(DB_SERVICE, DB_USER, DB_PASS, DB_DATABASE);    
+    public function __construct($host, $user, $password, $dbname) {  
+        $this->conn = new mysqli($host, $user, $password, $dbname);  
+        if ($this->conn->connect_error) {  
+            die(json_encode(['success' => false, 'errors' => ['Database connection failed!']]));  
+        }  
     }  
 
     public function runQuery($query, $params = []) {  
         $stmt = $this->conn->prepare($query);  
-        
         if ($params) {  
-            $types = '';  
-    
-            foreach ($params as $param) {  
-                if (is_int($param)) {  
-                    $types .= 'i';   
-                } elseif (is_double($param)) {  
-                    $types .= 'd';   
-                } else {  
-                    $types .= 's';  
-                }  
-            }  
-    
-            $stmt->bind_param($types, ...$params);  
+            $stmt->bind_param(str_repeat('s', count($params)), ...$params);  
         }  
         $stmt->execute();  
-        return $stmt->get_result();  
-    }
+        return $stmt;  
+    }  
 
     public function checkExists($query, $params = []) {  
-        $stmt = $this->conn->prepare($query);  
-        
-        if ($params) {  
-            $types = '';  
-    
-            foreach ($params as $param) {  
-                if (is_int($param)) {  
-                    $types .= 'i';   
-                } elseif (is_double($param)) {  
-                    $types .= 'd';
-                } else {  
-                    $types .= 's';  
-                }  
-            }  
-    
-            $stmt->bind_param($types, ...$params);  
-        }  
-    
-        $stmt->execute();  
-        $result = $stmt->get_result();  
-        return $result->num_rows === 0;  
+        $stmt = $this->runQuery($query, $params);  
+        return $stmt->get_result()->num_rows > 0;  
     }  
 
-    public function __destruct() {  
-        if ($this->conn) {  
-            $this->conn->close();  
-        }  
+    public function close() {  
+        $this->conn->close();  
     }  
 }  
-
-$mysql = new mySql();
+?>
