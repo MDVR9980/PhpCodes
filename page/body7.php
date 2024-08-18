3f8b53cd82ef107b8d0d00757c19ebb0c8db5cd0
MDVR9980 <mdvahhabrajaee@gmail.com>
2024-08-18 13:28:41 +0330

    Updated

---
+++ page/body7.php
@@ -0,0 +1,17 @@
+<?php
+echo "		
+    <center>
+        <div>
+            <h4>Staffuser Dashboard</h4><br/>
+        </div>	
+    </center>
+
+    <div class='div-form-btn'>
+        <form method='post'>
+            <div>
+                <input type='submit' class='submit-btn' name='btn-to-login' value='Exit' />
+            </div>   
+        </form>
+    <div>
+
+    	";
---
+++ page/dashboard3.php
@@ -0,0 +1,13 @@
+<?php
+include('../lib/connect.php');
+include('../lib/boot.php');
+include('header.php');
+?>
+
+<?php
+include('body7.php');
+?>
+
+<?php
+include('footer.php');
+?>
